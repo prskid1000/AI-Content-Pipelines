aa2b62075762d43696be003710e7480273f9939d
Prithwiraj Samanta <prskid1000@gmail.com>
2026-01-19 13:34:28 +0530

    updates

--- comfyui.bat
+++ comfyui.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
-python ComfyUI\main.py --listen --disable-pinned-memory --async-offload 16 --use-flash-attention
+python ComfyUI\main.py --listen --disable-pinned-memory --async-offload 16 --use-flash-attention --lowvram --force-fp16 --bf16-unet
 pause
