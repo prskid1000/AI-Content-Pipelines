71acd37d07068c266f1c1456ad5120e82b344b09
Prithwiraj Samanta <prskid1000@gmail.com>
2026-03-08 07:58:20 +0530

    updates

--- comfyui.bat
+++ comfyui.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
-python ComfyUI\main.py --listen --async-offload 16 --cache-none --reserve-vram 0.7
+python ComfyUI\main.py --listen --async-offload 16 --cache-none --reserve-vram 0.3
 pause
